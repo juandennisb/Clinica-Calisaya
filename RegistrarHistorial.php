3976f0cdcebeb8af4ad0bf720f06e3b9d49a85f4
juandennisb <juandennisb17@gmail.com>
2021-09-29 22:19:50 -0400

    Registro

--- RegistrarHistorial.php
+++ RegistrarHistorial.php
@@ -43,7 +43,7 @@
             
             $error .= '<i style="color: white;">Historial registrado exitosamente</i>';
         }
-//Ingresando datos 
+//Ingresando datos del ususario 
         if ($error == ''){
             $statement = $conexion->prepare('INSERT INTO historial (id, codigo, nombre, carnet, tratamiento) VALUES (null, :codigo, :nombre, :carnet, :tratamiento)');
             $statement->execute(array(
